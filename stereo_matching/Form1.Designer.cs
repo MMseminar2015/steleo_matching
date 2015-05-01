09235397cac2bc4ef0ca35febba1fc5b98a9cbd2
0kazuya <ohara.kazuya@ist.osaka-u.ac.jp>
2015-05-01 12:40:05 +0900

    text

--- stereo_matching/Form1.Designer.cs
+++ stereo_matching/Form1.Designer.cs
@@ -37,9 +37,9 @@
             this.label1.AutoSize = true;
             this.label1.Location = new System.Drawing.Point(132, 102);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(35, 12);
+            this.label1.Size = new System.Drawing.Size(25, 12);
             this.label1.TabIndex = 0;
-            this.label1.Text = "label1";
+            this.label1.Text = "text";
             // 
             // button1
             // 
