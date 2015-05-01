6023de83a11ec843fcad5040563f2d746cd942da
adach1 <adachi.tatsuya@ist.osaka-u.ac.jp>
2015-05-01 12:24:25 +0900

    Hello World

--- stereo_matching/Form1.cs
+++ stereo_matching/Form1.cs
@@ -16,5 +16,10 @@ namespace stereo_matching
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            label1.Text = "Hello World";
+        }
     }
 }
